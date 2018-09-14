5eddc04e1ccc104ae319d892caab1c9c057a123a
Sylvain Masia <sylvain.masia@etu.unice.fr>
2018-09-14 15:14:25 +0200

    Slice n°2

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -2,16 +2,18 @@ package jeu;
 
 import java.util.Scanner;
 
-
 public class Jeu {
 	private Scanner s;
-	private Joueur j1;
+	private Joueur j1, j2;
 	
 	public void lancement(String[] args) {
 		System.out.println("Bienvenue sur la Main de Poker");
-		this.j1 = new Joueur();
-		System.out.println("Veuillez rentrer les cartes du joueur");
+		this.j1 = new Joueur("Jean");
+		this.j2 = new Joueur("Pierre");
+		System.out.println("Veuillez rentrer les cartes de "+j1.getNom()+"(main 1)");
 		saisieCarte(args, j1);
+		System.out.println("Veuillez rentrer les cartes de "+j2.getNom()+"(main 2)");
+		saisieCarte(args, j2);
 		fin();
 		
 		
@@ -20,14 +22,17 @@ public class Jeu {
 	public void saisieCarte(String[] args, Joueur j) {
 		s = new Scanner(System.in);
 		String main = s.nextLine();
-		s.close();
+		s.reset();
 		j.setMain(main);
+
 		
 	}
 	
 	public void fin() {
-		System.out.println("Le joueur a gagné avec la main:"+j1.getMain());
+		System.out.println("Le joueur j1 a gagné avec la main:"+j1.getMain().toString());
+		s.close();
 	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

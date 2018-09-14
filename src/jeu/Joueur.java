46a58f9dc262808843479a4743fd0ac40f493f5f
Sylvain Masia <sylvain.masia@etu.unice.fr>
2018-09-14 15:25:48 +0200

    Slice n°3 en cours

--- src/jeu/Joueur.java
+++ src/jeu/Joueur.java
@@ -1,24 +1,34 @@
 package jeu;
 
+import java.util.ArrayList;
+
 public class Joueur {
 	private String nom;
 	
-	private String main;
+	private ArrayList<Carte> main;
 	
 	public Joueur(String nom) {
 		this.nom = nom;
+		this.main = new ArrayList<Carte>();
+	}
+		
+	public Joueur(ArrayList<String> main) {
+		this.main = new ArrayList<Carte>();
+		setMain(main);
 	}
 	
 	public String getNom() {
 		return nom;
 	}
 	
-	public String getMain() {
+	public ArrayList<Carte> getMain() {
 		return main;
 	}
 	
-	public void setMain(String main) {
-		this.main = main;
+	public void setMain(ArrayList<String> main) {
+		for(String carte : main) {
+			this.main.add(new Carte(Integer.parseInt(carte)));
+		}
 	}
 
 }

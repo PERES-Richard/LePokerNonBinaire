24b271b09d004896b5b52dd23151cd9098b42105
Sacha <Sacha@SurfaceProSacha.home>
2018-09-18 01:19:37 +0900

    slice 3

--- src/jeu/Joueur.java
+++ src/jeu/Joueur.java
@@ -12,7 +12,7 @@ public class Joueur {
 		this.main = new ArrayList<Carte>();
 	}
 		
-	public Joueur(ArrayList<String> main) {
+	public Joueur(ArrayList<String> main) throws CarteInvalideException{
 		this.main = new ArrayList<Carte>();
 		setMain(main);
 	}
@@ -25,10 +25,67 @@ public class Joueur {
 		return main;
 	}
 	
-	public void setMain(ArrayList<String> main) {
+	public void setMain(ArrayList<String> main) throws CarteInvalideException{
 		for(String carte : main) {
-			this.main.add(new Carte(Integer.parseInt(carte)));
+				this.main.add(convertToCarte(carte));
 		}
 	}
-
+	
+	public Carte convertToCarte(String carte) throws CarteInvalideException {
+		int valeur;
+		Couleur c;
+		if (carte.length()==3) {
+			c = findCouleur(carte.substring(1));
+			String v = carte.substring(0,1);
+			try {
+				valeur = Integer.valueOf(v);
+			}
+			catch(Exception e) {
+				switch(v) {
+					case "A" :
+						valeur = 14;
+						break;
+					case "R" : case "K" :
+						valeur = 13;
+						break;
+					case "D" : case "Q" :
+						valeur = 12;
+						break;
+					case "V" : case "J" :
+						valeur = 11;
+						break;
+					default :
+						throw new CarteInvalideException("Valeur invalide");
+				}
+			}
+		}
+		else if (carte.length()==4) {
+			String v = carte.substring(0,2);
+			if (Integer.valueOf(v)==10) {
+				c = findCouleur(carte.substring(2));
+				valeur = 10;
+			}
+			else
+				throw new CarteInvalideException();
+		}
+		else
+			throw new CarteInvalideException();
+		
+		return new Carte(c, valeur);
+	}	
+	
+	public static Couleur findCouleur(String s) throws CarteInvalideException{
+		switch(s) {
+			case "Pi" :
+				return Couleur.Pique;
+			case "Co" :
+				return Couleur.Coeur;
+			case "Tr" :
+				return Couleur.Trefle;
+			case "Ca" :
+				return Couleur.Carreau;
+			default :
+				throw new CarteInvalideException("Couleur inexistante");
+		}
+	}
 }

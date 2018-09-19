737d969458cee5e4cbd9ca055ed2495042f404d1
Armand <armand-boulanger@etu.univ-cotedazur.fr>
2018-09-19 17:39:23 +0200

    Modification CarteLaPlusHaute

--- src/jeu/CarteLaPlusHaute.java
+++ src/jeu/CarteLaPlusHaute.java
@@ -3,27 +3,41 @@ package jeu;
 import java.util.ArrayList;
 
 public class CarteLaPlusHaute extends Combinaison {
-	
+	private Carte carteHaute;
+
+	public CarteLaPlusHaute(ArrayList<Carte> main) {
+		Carte carteH, carte;
+		carteH = main.get(0);
+		for (int i=1; i<main.size(); i++) {
+			carte = main.get(i);
+			if (carteH.getValeur() < carte.getValeur())
+				carteH = carte;
+		}
+		this.carteHaute = carteH;
+	}
+
+	public Carte getCarteHaute() {
+		return this.carteHaute;
+	}
+
+	/*
 	public int compare(ArrayList<Carte> m1, ArrayList<Carte> m2) {
 		int maxm1=0; int i1=0; int i2=0; int maxm2=0;
-		for(Carte c : m1) { 
+		for(Carte c : m1) {
 			if (maxm1 < c.getValeur()) { maxm1 = c.getValeur(); i1 = m1.indexOf(c);}
 		}
-		for(Carte c2 : m2) { 
+		for(Carte c2 : m2) {
 			if (maxm2 < c2.getValeur()) { maxm2 = c2.getValeur(); i2=m1.indexOf(c2);}
 		}
 		if(maxm2 == maxm1) {
 			m1.remove(i1);
 			m2.remove(i2);
 			compare(m1,m2);
-		}	
-		if (maxm2 > maxm1) 
-			return maxm2; 
-		else 
+		}
+		if (maxm2 > maxm1)
+			return maxm2;
+		else
 			return maxm1;
-		
 	}
-	
-	
-
+	*/
 }

651786f8cae31bc7289ba484bb71f29f6d5e141e
Richard Peres <riritueur@gmail.com>
2018-09-19 16:19:42 +0200

    Update carte et jeu

--- src/jeu/Carte.java
+++ src/jeu/Carte.java
@@ -12,10 +12,9 @@ public class Carte {
 		this.valeur = valeur;
 	}
 	
-	public Carte(String main) {
-		this.couleur = Couleur.Carreau;
-		this.valeur = 0;
-		//TODO
+	public Carte(String valeur, String couleur) {
+		setCouleur(couleur);
+		setValeur(valeur);
 	}
 	
 	public int getValeur() {
@@ -25,6 +24,16 @@ public class Carte {
 	public void setValeur(int valeur) {
 		this.valeur = valeur;
 	}
+	
+	public void setValeur(String valeur) {
+		if(valeur.length() > 2 || valeur.length() < 0) {
+			
+		}
+		
+		if(valeur.length() == 1) {
+			
+		}
+	}
 
 	public Couleur getCouleur() {
 		return couleur;
@@ -32,6 +41,20 @@ public class Carte {
 
 	public void setCouleur(Couleur couleur) {
 		this.couleur = couleur;
+	}
+	
+	public void setCouleur(String couleur) {
+		switch(couleur) {
+		case "Pi" :
+			this.couleur =  Couleur.Pique;
+		case "Co" :
+			this.couleur =  Couleur.Coeur;
+		case "Tr" :
+			this.couleur =  Couleur.Trefle;
+		case "Ca" :
+			this.couleur =  Couleur.Carreau;
+		default : // TODO exception
+		}
 	}
 	
 	public String getSymbol() {
@@ -55,6 +78,20 @@ public class Carte {
 
 	public static ArrayList<Carte> convertToCarte(String main) {
 		
+		ArrayList<Carte> mainCartes = new ArrayList<Carte>();
+		
+		String[] cartes = main.split(" ");
+		if(cartes.length > Jeu.maxCarte)  { /*TODO exception "trop de carte"}*/}
+		
+		for(String carte : cartes) {
+			if(carte.length() > 4 || carte.length() < 3) { /*TODO exception "pas une carte"}*/}
+			
+			String couleur = main.substring(main.length()-3, main.length()-1);
+			String valeur = main.substring(main.length()-3, main.length()-1);
+			
+			mainCartes.add(new Carte(valeur, couleur));
+		}
+		
 		return null;
 	}
 	
@@ -73,21 +110,6 @@ public class Carte {
 			case "V" : case "J" :
 				valeur = 11;
 				break;
-		}
-				
-				
-		public Couleur findCouleur(String s){
-			switch(s) {
-				case "Pi" :
-					return Couleur.Pique;
-				case "Co" :
-					return Couleur.Coeur;
-				case "Tr" :
-					return Couleur.Trefle;
-				case "Ca" :
-					return Couleur.Carreau;
-				default : return null; // TODO exception
-			}
 		}
 		
 	 */

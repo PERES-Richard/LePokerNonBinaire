99f5d591537ea0708f87f47cebfd4ffb75a48c2a
Armand <armand-boulanger@etu.univ-cotedazur.fr>
2018-09-24 22:10:44 +0200

    Initialisation ok, avec un nouveau setMain (un vrai setter) et donc un doMain (qui retourne un Array de cartes)

--- src/jeu/Joueur.java
+++ src/jeu/Joueur.java
@@ -6,44 +6,50 @@ public class Joueur {
 	private String nom;
 	private ArrayList<Carte> main;
 	private ArrayList<Combinaison> comb;
-	
-		
+
+	public Joueur(String nom){
+		this.nom = nom;
+	}
+
 	public Joueur(String nom, String main){
 		this.nom = nom;
-		this.main = setMain(main);
+		this.main = doMain(main);
 		this.comb = findCombinaison(this.main);
 	}
 
+	public void setMain(String main){
+		this.main = Carte.convertToCarte(main);
+	}
 
 	public String getNom() {
 		return nom;
 	}
-	
+
 	public ArrayList<Carte> getMain() {
 		return main;
 	}
-	
+
 	public ArrayList<Combinaison> getCombinaison() {
 		return comb;
 	}
-	
-	public ArrayList<Carte> setMain(String main){
+
+	public ArrayList<Carte> doMain(String main){
 		return Carte.convertToCarte(main);
 	}
-	
+
 	public ArrayList<Combinaison> findCombinaison(ArrayList<Carte> main) {
-		
+
 		ArrayList<Combinaison> temp = new ArrayList<Combinaison>();
-		
+
 		CarteLaPlusHaute cHaute = new CarteLaPlusHaute(main);
 		temp.add(cHaute);
-		
+
 		//Ici on pourra ajouter les combinaisons dans l'ordre inverse de puissance
-		//comme ça on comparera le premier element de la liste
-		
+		//comme ï¿½a on comparera le premier element de la liste
+
 		return temp;
 	}
-	
+
 	public boolean dansMain(Carte carte) {
 		return this.main.contains(carte);
 	}
